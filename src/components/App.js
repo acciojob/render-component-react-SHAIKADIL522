e2c0f9894d6dec5d306470dac3d9a449fd178bf3
ADIL SHAIK <adilshaik9112@gmail.com>
2026-04-02 11:48:18 +0530

    Fixed test case by removing extra div wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
-import React, {Component, useState} from "react";
+import React from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main">
-      <p>I am learning React. My life is getting better.</p>
-    </div>
-  )
+    <p>I am learning React. My life is getting better.</p>
+  );
 }
-
 
 export default App;
