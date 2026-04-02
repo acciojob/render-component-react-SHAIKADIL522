ffd4cb2d9bb77b5b35a21cd832bc7c09a18726a0
ADIL SHAIK <adilshaik9112@gmail.com>
2026-04-02 19:29:57 +0530

    changed the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import '../styles/App.css';
 
 const App = () => {
   return (
     <p>I am learning React. My life is getting better.</p>
   );
-}
+};
 
 export default App;
